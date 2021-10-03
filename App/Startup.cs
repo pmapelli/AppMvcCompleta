0ea57446a77c7d58114433da73fa21a87f1d4755
Pedro Mapelli <pedro.mapelli@ibpt.org.br>
2021-10-03 13:58:46 -0300

    feat: Mapeando entitades para o banco de dados

--- App/Startup.cs
+++ App/Startup.cs
@@ -1,17 +1,12 @@
 using App.Data;
+using Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace App
 {
@@ -28,8 +23,11 @@ namespace App
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
---
+++ Bussines/Models/TipoFornecedor.cs
@@ -0,0 +1,8 @@
+﻿namespace Bussines.Models
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
